fbe97513223703e80a0628d30a6a6a776976f46d
Steve Sparks <15949542+StevenSSparks@users.noreply.github.com>
2026-06-18 17:05:05 -0500

    cask: oliver 0.1.586

--- Casks/oliver.rb
+++ Casks/oliver.rb
@@ -1,6 +1,6 @@
 cask "oliver" do
-  version "0.1.584"
-  sha256 "8506b3b92ce20fe963a55df316992f4a225fb96bd0950e481aaf5243af84b536"
+  version "0.1.586"
+  sha256 "3de407b16cd5a3020149ff82f931f440a675d8d64f394ed27530824c2c48b9a3"
 
   url "https://github.com/ta-tools/oliver-release/releases/download/v#{version}/Oliver-#{version}-osx-arm64.dmg"
   name "Oliver"
